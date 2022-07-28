ac10aebda60ff6aedb91e18a08ef5c22b3e82bc2
Marcin Salamon <marcin.salamon@motorolasolutions.com>
2022-07-28 11:12:37 +0200

    Add pointers to rtv products

--- stock.cpp
+++ stock.cpp
@@ -4,11 +4,23 @@
 Stock::Stock() {
 	
 
-		Product Telewizor(2000.00, "Telewizor","RTV", "Samsung");
+		Product Telewizory(2000.00, "Telewizor","RTV", "Samsung");
 		Product Telefony(300.00, "Telefon", "RTV", "LG");
 		Product Laptopy(3000.00, "Laptop", "RTV", "Samsung");
 		Product Sluchawki(100.00, "Sluchawki", "RTV", "LG");
 		Product Radio(200.00, "Radio", "RTV", "Bosch");
+
+		Product* Telwizory_ptr{ &Telewizory };
+		Product* Telefony_ptr{ &Telefony };
+		Product* Laptopy_ptr{ &Laptopy };
+		Product* Sluchawki_ptr{ &Sluchawki };
+		Product* Radio_ptr{ &Radio };
+
+		storage.insert({ Telwizory_ptr, 7 });
+		storage.insert({ Telefony_ptr, 5 });
+		storage.insert({ Laptopy_ptr, 8 });
+		storage.insert({ Sluchawki_ptr, 6 });
+		storage.insert({ Radio_ptr, 4 });
 
 	storage.insert(pro)
 }
