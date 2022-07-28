52749f97add44d41cce4a7d78f3230eac40c845f
Marcin Salamon <marcin.salamon@motorolasolutions.com>
2022-07-28 11:43:04 +0200

    change name to lowwer

--- stock.cpp
+++ stock.cpp
@@ -23,23 +23,23 @@ Stock::Stock() {
 	storage.insert({ Sekator_ptr, 5 });
 	storage.insert({ Sekator_ptr, 6 });
 
-		Product Telewizory(2000.00, "Telewizor","RTV", "Samsung");
-		Product Telefony(300.00, "Telefon", "RTV", "LG");
-		Product Laptopy(3000.00, "Laptop", "RTV", "Samsung");
-		Product Sluchawki(100.00, "Sluchawki", "RTV", "LG");
-		Product Radio(200.00, "Radio", "RTV", "Bosch");
+	Product Telewizory(2000.00, "Telewizor","RTV", "Samsung");
+	Product Telefony(300.00, "Telefon", "RTV", "LG");
+	Product Laptopy(3000.00, "Laptop", "RTV", "Samsung");
+	Product Sluchawki(100.00, "Sluchawki", "RTV", "LG");
+	Product Radio(200.00, "Radio", "RTV", "Bosch");
 
-		Product* Telwizory_ptr{ &Telewizory };
-		Product* Telefony_ptr{ &Telefony };
-		Product* Laptopy_ptr{ &Laptopy };
-		Product* Sluchawki_ptr{ &Sluchawki };
-		Product* Radio_ptr{ &Radio };
+	Product* Telwizory_ptr{ &Telewizory };
+	Product* Telefony_ptr{ &Telefony };
+	Product* Laptopy_ptr{ &Laptopy };
+	Product* Sluchawki_ptr{ &Sluchawki };
+	Product* Radio_ptr{ &Radio };
 
-		storage.insert({ Telwizory_ptr, 7 });
-		storage.insert({ Telefony_ptr, 5 });
-		storage.insert({ Laptopy_ptr, 8 });
-		storage.insert({ Sluchawki_ptr, 6 });
-		storage.insert({ Radio_ptr, 4 });
+	storage.insert({ Telwizory_ptr, 7 });
+	storage.insert({ Telefony_ptr, 5 });
+	storage.insert({ Laptopy_ptr, 8 });
+	storage.insert({ Sluchawki_ptr, 6 });
+	storage.insert({ Radio_ptr, 4 });
 
 	Product lodowki(1500.00, "Lodowka", "AGD", "LG");
 	Product mikrofalowki(600.00, "Mikrofalowka", "AGD", "Bosch");
@@ -52,26 +52,28 @@ Stock::Stock() {
 	Product* odkurzacze_ptr = &odkurzacze;
 	Product* piekarniki_ptr = &piekarniki;
 	Product* pralki_ptr = &pralki;
-		Product Samochodziki(20.00, "Samochod", "TOYS", "Sharp");
-		Product Klocki(200.00, "Klocki", "TOYS", "LG");
-		Product Lalki(40.00, "Lalka", "TOYS", "Samsung");
-		Product Puzzle(30.00, "Puzzle", "TOYS", "Bosch");
-		Product Maskotki(100.00, "Maskotka", "TOYS", "Sharp");
+
+	Product Samochodziki(20.00, "Samochod", "TOYS", "Sharp");
+	Product Klocki(200.00, "Klocki", "TOYS", "LG");
+	Product Lalki(40.00, "Lalka", "TOYS", "Samsung");
+	Product Puzzle(30.00, "Puzzle", "TOYS", "Bosch");
+	Product Maskotki(100.00, "Maskotka", "TOYS", "Sharp");
 
 	storage.insert({ lodowki_ptr,5 });
 	storage.insert({ mikrofalowki_ptr,5 });
 	storage.insert({ odkurzacze_ptr,5 });
 	storage.insert({ piekarniki_ptr,5 });
 	storage.insert({ pralki_ptr,5 });
-		Product* Samochodziki_ptr{ &Samochodziki };
-		Product* Klocki_ptr{ &Klocki };
-		Product* Lalki_ptr{ &Lalki};
-		Product* Puzzle_ptr{ &Puzzle};
-		Product* Maskotki_ptr{ &Maskotki };
 
-		storage.insert({ Samochodziki_ptr, 7 });
-		storage.insert({ Klocki_ptr, 5 });
-		storage.insert({ Lalki_ptr, 8 });
-		storage.insert({ Puzzle_ptr, 6 });
-		storage.insert({ Maskotki_ptr, 4 });
+	Product* Samochodziki_ptr{ &Samochodziki };
+	Product* Klocki_ptr{ &Klocki };
+	Product* Lalki_ptr{ &Lalki};
+	Product* Puzzle_ptr{ &Puzzle};
+	Product* Maskotki_ptr{ &Maskotki };
+
+	storage.insert({ Samochodziki_ptr, 7 });
+	storage.insert({ Klocki_ptr, 5 });
+	storage.insert({ Lalki_ptr, 8 });
+	storage.insert({ Puzzle_ptr, 6 });
+	storage.insert({ Maskotki_ptr, 4 });
 }
